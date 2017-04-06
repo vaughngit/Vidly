29161e307cc430e3996e1d338a08900969e15a2b
Alvin <Alvin.vaughn@epsworks.com>
2017-04-05 22:44:27 -0500

    Changes related to review and notations for future reference

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,21 +13,22 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // attribute routing introduced in MVC 5 and replaces the frigle routes.MapRoute() convention commented out below - see 13 Atribute Routing
-            // Atrtibute routing allow you to apply several contraints to routing conditions as seen in route specified in Line 14 of the MoviesController.cs 
-             routes.MapMvcAttributeRoutes();  // enable attribute routing for mvc 5 and above 
-
             /*
-            // Convention routing 
+            // routes.MapRoute() Convention routing Lession 12
             routes.MapRoute(
                 "MoviesByReleaseDate",
                 "movies/released/{year}/{month}",
-                new {Controller = "Movies", action = "ByReleaseDate"}, // anonymous objective for default route  
-              //new { year = @"\d{4}", month = @"\d{2}"}  // use regular expression to apply constrants (\d represents a digit, {4} and {2} represents the number of digits. Use the @ sign to escape the regular backslash escape char
-                new { year = @"2015 | 2016", month = @"\d{2}" } // regular expression to constrain the query to two specific years
+                new {Controller = "Movies", action = "ByReleaseDate"}, // anonymous objective for defining paramenter  
+                      //new { year = @"\d{4}", month = @"\d{2}"}  // use regular expression to apply constrants (\d represents a digit, {4} and {2} represents the number of digits. Use the @ sign to escape the regular backslash escape char
+                new { year = @"2015 | 2016", month = @"\d{2}" } // regular expression to constrain the query to two specific years plus the month contraint 
                 );
 
             */
+
+            // attribute routing introduced in MVC 5 and replaces the frigle routes.MapRoute() convention commented out above - see 13 Atribute Routing
+            // Atrtibute routing allow you to apply several contraints to routing conditions as seen in route specified in the [route()] of the MoviesController.cs 
+            routes.MapMvcAttributeRoutes();  // enables attribute routing for mvc 5 and above 
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,18 +11,41 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        
-       
+
+
+        // Action Parameters: Lesson 11
+        // this action will be called when we navigate to movies index page:
+        public ActionResult Index(int? pageIndex, string sortBy)
+            // add ? after parm type to make it nullable thereby making it an optional parameter. the softBy parm is a string type which is a reference type that is nullable by default 
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1; // if pageIndex doesnt have a value initialize it to 1
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name"; // if string is null or whitespace, initialize it to Name 
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy)); // space the 
+        }
+
+
+        // url for this ActionResult is localhost:port/movies/edit?id=#
+        public ActionResult Edit(int? id)
+        {
+            return Content("id=" + id);
+        }
+
+
+
+
 
         // GET: Movies/Random
-        //public ActionResult Random() // Parent class
-        //public ViewResult Random() // subclass of ActionResult
-        public ActionResult Random()   // This function passes data to the view that calls it 
+        public ActionResult Random() // This function passes data to the view that calls it 
         {
             // create instance of the movie model
-            var movie = new Movie() {Name = "Shrek!"}; //create an instance of the movie model and assign a value to the name property of this instances 
+            var movie = new Movie() {Name = "Shrek!"};
+            //create an instance of the movie model and assign a value to the name property of this instances 
 
-            // create instance of customer viewView
+            // create instance of customer view
             var customers = new List<Customer>
             {
                 new Customer {Name = "Customer 1"},
@@ -35,44 +58,42 @@ namespace Vidly.Controllers
                 Customers = customers
             };
 
+            /*
+             Other Action Result Types: 
+                  return Content("Hello World!");                                                 // ContentResult
+                  return HttpNotFound();                                                         // HttpNotFoundResult returns 404 page not found page
+                  return new EmptyResult();                                                     // EmptyResult requires a new declaration since it doesnt have a helper method like the other ActionTypes 
+                  return RedirectToAction("Index", "Home", new {page =1, sortby = "name"});    //RedirectToReouteResult - see below
+                                /*
+                                return RedirectToAction(NameOfAction, Controller);
+                                return RedirectToAction(NameOfAction, Controller, new {page = 1, sortby = "name"});  use anonamous object to pass aruguments to a page :
+                                page url appears as: http://localhost:port#/?page=1&sortby=name
+                                */
 
-            // return Content("Hello World!");
-            //  return HttpNotFound(); // returns 404 page not found page
-            //return new EmptyResult();    
-            // return RedirectToAction("Index", "Home", new {page =1, sortby = "name"});
-            
-            //return View(movie);  // returns move object
-          return View(viewModel);  // instead of sending the movie as in the line above, we send the viewModel
+            //  return View(movie);                                                          //ViewResult returns move object
+
+            return View(viewModel); // instead of sending the movie as in the line above, we send the viewModel
         }
 
-        // apply attribute routing method
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]  /* attribute routing allows you to apply multiple constraint types in addition to regex
-                                                                              other constraints are min, max, minlength, int, float, guid, etc for more google 'ASP.NET MVC attribute route constraints'
-                                                                              */
 
+
+
+
+        // Attribute routing Lesson: 13
+        /* attribute routing allows you to apply multiple constraint types in addition to regex other constraints are min, max, minlength, int, float, guid, etc 
+         * for more google 'ASP.NET MVC attribute route constraints'                                         
+        * Attribute routing is enabled in the RouteConfig.cs file within the App_Start folder, syntax is: routes.MapMvcAttributeRoutes();
+        */
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        // use regular expression to apply constrants (\\d represents a digit, and {2} represents the number of digits.
         public ActionResult ByReleaseDate(int year, int month)
+            //The Attribute route does not have to match the proceding method name. 
         {
             return Content(year + "/" + month);
         }
 
-        /*
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
 
-        // this action will be called when we navigate to movies:
-        public ActionResult Index(int? pageIndex, string sortBy) // add ? after parm type to make it nullable thereby making it an optional parameter. the softBy parm is a string type which is a reference type that is nullable by default 
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-        */
     }
 }
