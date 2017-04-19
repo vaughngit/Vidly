c02f2899d9226bd6c151881c2440e7c63f1498e6
Alvin <Alvin.vaughn@epsworks.com>
2017-04-19 07:37:54 -0500

    Added customers controller

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
